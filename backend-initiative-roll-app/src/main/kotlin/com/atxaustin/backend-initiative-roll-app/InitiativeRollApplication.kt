e0b195d76bc508164cd6ece9bd9b13e660f39deb
Austin Howard <106770402+ahowardfigo@users.noreply.github.com>
2025-05-13 16:13:20 -0500

    initial commit

---
+++ backend-initiative-roll-app/src/main/kotlin/com/atxaustin/backend-initiative-roll-app/InitiativeRollApplication.kt
@@ -0,0 +1,11 @@
+package com.atxaustin.dnd_roller_app_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InitiativeRollApplication
+
+fun main(args: Array<String>) {
+	runApplication<InitiativeRollApplication>(*args)
+}
